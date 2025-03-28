cb5b736ba805ccb48c29483f104ac6ab2c977c3f
Amene <layla_1380@yahoo.com>
2025-03-28 14:42:55 -0500

    Create AppContext to manage user, plants, and categories state with session check and data fetch

---
+++ client/src/components/AppContext.js
@@ -0,0 +1,46 @@
+
+import React, { createContext, useState, useEffect} from 'react'
+
+const AppContext = createContext()
+export const AppProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [plants, setPlants] = useState([])
+    const [categories, setCategories] = useState([])
+    function setPlantsData(plantsData) {
+        setPlants(plantsData)
+    }
+
+
+    useEffect(() => {
+        
+        fetch('/check_session')
+            .then(res => {
+                if (!res.ok) {
+                    console.log("Raw Response:", res)
+
+                throw new Error('Failed to fetch data.')
+                }
+                return res.json()
+        })
+            .then(data => {
+                console.log("Parsed JSON Data:", data); // Log the parsed JSON data
+            
+                if (data) {
+                    setUser(data)
+                    setCategories(data.categories)
+                }
+
+                
+        })
+        .catch(e=> console.error(e))
+    }, [])
+
+   
+    return (
+        <AppContext.Provider value={{ user, setUser, plants, setPlants, categories, setCategories }}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export default AppContext
